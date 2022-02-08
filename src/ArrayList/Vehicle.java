fed5e96397c95ed6900b5268e3166ec5a917dbd4
Victor Jason Presher <presher@users.noreply.github.com>
2022-02-08 11:38:13 -0600

    Create Vehicle.java

---
+++ src/ArrayList/Vehicle.java
@@ -0,0 +1,57 @@
+package lesson2;
+
+public class Vehicle {
+	String make;
+	String model;
+	int price;
+	boolean fourWheelDrive;
+	
+	public Vehicle(String make, String model, int price, boolean fourWheelDrive) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.price = price;
+		this.fourWheelDrive = fourWheelDrive;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Vehicle [make=" + make + ", model=" + model + ", price=" + price + ", fourWheelDrive=" + fourWheelDrive
+				+ "]";
+	}
+
+
+
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isFourWheelDrive() {
+		return fourWheelDrive;
+	}
+	public void setFourWheelDrive(boolean fourWheelDrive) {
+		this.fourWheelDrive = fourWheelDrive;
+	}
+	
+	
+	
+	
+	
+	
+}
